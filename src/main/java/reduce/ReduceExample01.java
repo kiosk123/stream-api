51c84c3b82c89632afa870e8b02655ec931712f5
Heo Jong Tae <heo9910@naver.com>
2020-12-25 15:19:56 +0900

    .

--- src/main/java/reduce/ReduceExample01.java
+++ src/main/java/reduce/ReduceExample01.java
@@ -13,13 +13,13 @@ public class ReduceExample01 {
         System.out.println("종합점수 점수 : " + total);
         
         
-        List<String> ls = Arrays.asList("Box", "Simple", "Complex", "Robot");
         
         /*
          *  람다식을 기준으로 만족하는 데이터를 찾을 때까지 비교해가며 찾는다
             stream이 빈경우 빈 문자열 반환. 스트림이 비어있지 않은 경우 
                            첫번째 파라미터를 첫번째 비교대상으로 적용
         */
+        List<String> ls = Arrays.asList("Box", "Simple", "Complex", "Robot");
         String str = ls.stream().reduce("", (s1, s2) -> {
             if (s1.length() > s2.length()) {
                 return s1;
